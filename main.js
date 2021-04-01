090c298c45964a8c365e04c2faf04368dc2337a5
David Allen <dayvedallen@gmail.com>
2021-04-01 13:27:56 -0400

    character array demo

--- main.js
+++ main.js
@@ -1,12 +1,65 @@
-console.log( "hi kramer")
+// console.log( "hi kramer")
 
 // square brackets array literal 
 
-let list = ["Tom", "Dick", "And","Harry"]
+// let list = ["Tom", "Dick", "And","Harry"]
 
 
-// element positions zero indexed. 
+// // element positions zero indexed. 
 
-console.log ([list[0] ])
+// console.log ([list[0] ])
+
+// let numberArray = [
+console.log( '######')
+console.log( 'Number Array')
+// console.log("Length: "   )
+// console.log(numberArray[numberArray.length-1])
+//     13, 99, 10, 451, 44
+// ]
+let numberArray = []
+
+numberArray[0] = 13
+numberArray[1] = 99
+numberArray[2] = 10
+numberArray[3] = 451
+numberArray[4] = 44 
+
+numberArray[4] = 44 
 
 
+console.log( " The last one is ")
+console.log(numberArray.length-1 )
+console.log( numberArray[numberArray.length-1] )
+
+
+
+
+// char array 
+let charArray = []
+
+charArray.push('a')
+charArray.push('b')
+charArray.push('c')
+
+console.log(charArray)
+
+
+let index = prompt('enter char index')
+let value = prompt('enter char index val')
+
+console.log(index)
+// update the index 
+charArray[index] = value 
+console.log( charArray[0])
+
+let sentence = [
+
+]
+
+// return sumNumbers[0] + 
+
+
+let sum = numbers[0] + 
+numbers 
+
+//could loop
